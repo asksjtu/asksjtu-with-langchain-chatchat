0a3838dce40b7f6d0d04f61aa6737c8479763941
truco <3866-truc0@users.noreply.test50k.gitlab.cn>
2023-10-11 00:22:13 +0800

    fix(asksjtu/cli): update usual name and error message of asksjtu_cli file

--- asksjtu_cli.py
+++ asksjtu_cli.py
@@ -7,15 +7,15 @@ from asksjtu_cli.update_stu_noti import update_stu_noti
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--kb-name', default='常用数据库')
+    parser.add_argument('--kb-name', default='常用知识库')
     return parser.parse_args()
 
 
 def update_stu_noti_handler(kb_name: str):
     kbs = list_kbs().data
-    print(kbs)
     if kb_name not in kbs:
-        raise ValueError(f"知识库 {kb_name} 不存在")
+        kbs_str = ', '.join(kbs)
+        raise ValueError(f"知识库 {kb_name} 不存在，可用知识库：{kbs_str}")
     return update_stu_noti(kb_name=kb_name)
 
 
